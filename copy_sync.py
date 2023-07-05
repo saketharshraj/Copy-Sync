3a8fcd7348db2010ae752b121d21c88e8867a9f4
Harsh Raj <harsh.smartters@gmail.com>
2023-07-05 15:41:33 +0530

    File creation error handled

--- copy_sync.py
+++ copy_sync.py
@@ -10,15 +10,20 @@ def copy_paste(e):
     try:
         with open('clipboard.txt', 'r') as r:
             lines = r.readlines()
-            print(lines)
             for line in lines:
                 s = line
                 pyautogui.hotkey('ctrl', 'backspace')
                 pyautogui.write(s)
                 pyautogui.write(' ')
-    except e:
-        print("[CURRENT TYPING IS TERMINATED PROBABLY DUE TO SAFETY TRIGGER]")
-
+    except pyautogui.FailSafeException:
+        print("[CURRENT TYPING IS TERMINATED DUE TO SAFETY TRIGGER]")
+    except FileNotFoundError:
+        # create the file with some content in the directory
+        s = 'This file is automatically created by copy sync.\nCopy the content on your github repo clipboard with file name code.txt'
+        with open('clipboard.txt', 'w') as f:
+            f.write(s)
+        pyautogui.write(s)
+            
 
 def sync_content(e):
     try:
@@ -49,12 +54,12 @@ def terminate_program(e):
 
 
 
-app_version = '0.1'
+app_version = 'v1.1.0'
 km = key_manager.KeyManager()
 github_auth_token = km.get_github_key()
 if not github_auth_token:
-    print('No github auth token found. Exiting...')
-    exit(0)
+    print('Run the program again . . .')
+    exit()
 
 auth = Auth.Token(github_auth_token)
 g = Github(auth=auth)
@@ -65,7 +70,7 @@ print("Running")
 
 # Assign functions to hotkeys
 keyboard.on_press_key('f9', copy_paste)
-keyboard.on_press_key('f7', sync_content)
+keyboard.on_press_key('f8', sync_content)
 
 keyboard.on_press_key('f12', terminate_program)
 
